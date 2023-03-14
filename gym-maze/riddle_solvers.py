7adb34488bc176f398d3329189d33a903e9aa86b
Yousef Ashraf <bosayousef981@gmail.com>
2023-03-14 19:40:04 +0200

    server riddle solution

--- gym-maze/riddle_solvers.py
+++ gym-maze/riddle_solvers.py
@@ -8,51 +8,54 @@ import time
 import os
 from scapy.all import *
 from io import BytesIO
+import jwt
+from jwcrypto import jwk
+
 
 def binary_to_number(binary):
-        number=0
-        length=len(binary)
-        for i in range(0,length):
-            if(binary[i]=='1'):
-                number+=2**(length-i-1)
-        return number
+    number = 0
+    length = len(binary)
+    for i in range(0, length):
+        if(binary[i] == '1'):
+            number += 2**(length-i-1)
+    return number
+
 
 def cipher_solver(question):
     # calc time
     start = time.time()
-    text=question+"=="
-    newtext=base64.b64decode(text)
-    #101000111010101001100111010010000111100001101001011011001010100
-    newtext=str(newtext)
-    newtext=newtext[3:-2]
-    lefttext=newtext[0:-5]
-    righttext=newtext[-4:]
-    rightnumber=binary_to_number(righttext)
-    #print(rightnumber)
+    text = question+"=="
+    newtext = base64.b64decode(text)
+    # 101000111010101001100111010010000111100001101001011011001010100
+    newtext = str(newtext)
+    newtext = newtext[3:-2]
+    lefttext = newtext[0:-5]
+    righttext = newtext[-4:]
+    rightnumber = binary_to_number(righttext)
+    # print(rightnumber)
 
-    strings=[]
-    for i in range(0,63,7):
+    strings = []
+    for i in range(0, 63, 7):
         strings.append(lefttext[i:i+7])
 
-    ans=""
+    ans = ""
 
-    
     for i in strings:
-        ascii=binary_to_number(i)
-        if(ascii<=90):
-            ascii-=rightnumber
-            if(ascii<65):
-                ascii+=26
+        ascii = binary_to_number(i)
+        if(ascii <= 90):
+            ascii -= rightnumber
+            if(ascii < 65):
+                ascii += 26
         else:
-            ascii-=rightnumber
-            if(ascii<97):
-                ascii+=26
-        ascii=chr(ascii)
-        
-        ans+=ascii
+            ascii -= rightnumber
+            if(ascii < 97):
+                ascii += 26
+        ascii = chr(ascii)
+
+        ans += ascii
     end = time.time()
     print("Time to cipher: ", end - start)
-    return ans        
+    return ans
 
 
 def captcha_solver(question):
@@ -77,7 +80,7 @@ def pcap_solver(question):
     # calc time
     start = time.time()
     # Return solution
-    
+
     base64_string = question+"=="
     pcap_bytes = base64.b64decode(base64_string)
 
@@ -85,17 +88,16 @@ def pcap_solver(question):
 
     pcap_packets = rdpcap(BytesIO(pcap_bytes))
 
-    dns_packets = [packet for packet in pcap_packets if packet.haslayer("ICMP")]
+    dns_packets = [
+        packet for packet in pcap_packets if packet.haslayer("ICMP")]
 
     list = []
     for packet in dns_packets:
         list.append(packet["ICMP"].qd.qname.decode())
 
-
     def get_subdomain(domain):
         parts = domain.split('.')
         return parts[0] if len(parts) > 1 else ''
-
 
     list = sorted(list, key=get_subdomain)
     editedList = []
@@ -108,5 +110,32 @@ def pcap_solver(question):
 
 
 def server_solver(question):
-    # Return solution
-    pass
+    splitedToken = question.split('.')
+    headers = splitedToken[0]
+    payload = splitedToken[1]
+
+    def base64_padding(s):
+        return s + '=' * (4 - len(s) % 4)
+
+    decoded_bytes = base64.b64decode(base64_padding(headers))
+    decoded_string = decoded_bytes.decode('utf-8')
+    headers = json.loads(decoded_string)
+
+    decoded_bytes = base64.b64decode(base64_padding(payload))
+    decoded_string = decoded_bytes.decode('utf-8')
+    payload = json.loads(decoded_string)
+
+    new_payload = {
+        **payload,
+        "admin": True
+    }
+
+    private_key = jwk.JWK.generate(kty="RSA", size=2048, kid=headers["kid"])
+    public_key = private_key.export(False)
+
+    public_key = json.loads(public_key)
+
+    header = {'alg': 'RS256', 'kid': headers["kid"], 'jwk': public_key}
+    jwt_token = jwt.encode(new_payload, private_key.export_to_pem(
+        private_key=True, password=None), algorithm="RS256", headers=header)
+    return jwt_token
